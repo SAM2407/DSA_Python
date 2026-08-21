99c45500a04592dce1c6012872379425ff70ef60
Samman Gond <112507091+SAM2407@users.noreply.github.com>
2026-08-21 20:09:48 +0530

    Implement method to find max consecutive ones

---
+++ 485_Max_Consecutive_Ones.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count=0
+        ans=0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                count+=1
+            else:
+                ans= max(ans,count)
+                count=0
+        ans = max(ans,count)
+        return ans
+        
