7fba402b45addc5c3d6a3e55df942313629d04d4
Samman Gond <112507091+SAM2407@users.noreply.github.com>
2026-09-07 04:25:41 +0530

    Create 704_Binary_Search.py

---
+++ 704_Binary_Search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        start=0
+        end=n-1
+        while start<=end:
+            mid =(start+end)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                end = mid-1
+            else:
+                start = mid+1
+        
+        return -1
+        
