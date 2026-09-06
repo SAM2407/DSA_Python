25efe93a53c47125b9b4b64780d248858bb0ff11
Samman Gond <112507091+SAM2407@users.noreply.github.com>
2026-09-07 04:45:19 +0530

    Implement searchInsert method for binary search

---
+++ 35_Search_Insert_Position.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)-1
+        start =0
+        end=n
+        ans =n+1
+        while start<=end:
+            mid = (start +end)//2
+            if nums[mid]>=target:
+                ans = mid
+                end = mid-1
+            else:
+                start = mid+1
+        
+        return ans
+
+
+
+        
